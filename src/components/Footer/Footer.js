677ff4cbe386fe99d2d9eb201ceceb001e480fec
AkramDev1 <akramessaidi98@gmail.com>
2021-09-18 18:18:54 +0200

    add portfolio

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from "react-icons/ai";
+
+import { SocialIcons } from "../Header/HeaderStyles";
+import {
+  CompanyContainer,
+  FooterWrapper,
+  LinkColumn,
+  LinkItem,
+  LinkList,
+  LinkTitle,
+  Slogan,
+  SocialContainer,
+  SocialIconsContainer,
+} from "./FooterStyles";
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:+212642687846">+212658540116</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:akramessaidi98@gmail.com">
+          akramessaidi98@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/AkramDev1">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.linkedin.com/in/">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
+  );
+};
+
+export default Footer;
---
+++ src/constants/constants.js
@@ -0,0 +1,39 @@
+export const projects = [{
+        title: "Git Engine",
+        description: "While building it I've learned many advanced React & JavaScript topics, It was my first react project, as well as how to use Stripe for card transactions. On top of that.",
+        image: "/images/git.PNG",
+        tags: ["React"],
+        source: "https://github.com/AkramDev1/ReactProjectGithub",
+        id: 1,
+    },
+    {
+        title: "Memories",
+        description: "This is a code repository for the corresponding project. In this one, we're going to build and deploy a web site memories using React and Node.",
+        image: "/images/memorise.PNG",
+        tags: ["React", "Node", "MongoDB"],
+        source: "https://github.com/PUisk-AB1/Portfolio_react_next",
+        id: 2,
+    },
+    {
+        title: "DataTable (BackOffice)",
+        description: "This is a code repository for the corresponding project. In this one, we're going to build an application desktop to controle the frontOffice (CHU) using ElectronJs and NodeJs.",
+        image: "/images/dataTable.PNG",
+        tags: ["Electron", "Node", "Mysql"],
+        source: "https://github.com/AkramDev1/BackOffice-Electron.js",
+        id: 2,
+    },
+    {
+        title: "CHU (FrontOffice)",
+        description: "This is a code repository for the corresponding project. In this one, we're going to build and deploy a web site CHU  using React, Node and MySqlDB .",
+        image: "/images/chu.PNG",
+        tags: ["React", "Node", "Mysql"],
+        source: "https://github.com/AkramDev1/front-office_API",
+        id: 2,
+    },
+];
+
+export const TimeLineData = [
+    { year: 2020, text: "Started my journey" },
+    { year: 2021, text: "Started my first project" },
+    { year: 2022, text: "Coming soon..." },
+];
