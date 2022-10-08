b7872e71a71e7f9b0e37fe781f20ab2a77174cbe
ESAAIDI Akram <87911826+AkramDev1@users.noreply.github.com>
2022-10-08 19:08:31 +0100

    Update Technologies.js

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+
 import {
   Section,
   SectionDivider,
@@ -22,6 +23,7 @@ const Technologies = () => (
       I've worked with a range a technologies in the web development world.
     </SectionText>
     <List>
+  
       <ListItem>
         <picture>
           <DiReact size="3rem" />
@@ -30,10 +32,12 @@ const Technologies = () => (
           <ListTitle>Front-End</ListTitle>
           <ListParagraph>
             Experience with <br />
-            React.js And Electron.js
+            React.js And Electron.js<br />
+            And Angular.js
           </ListParagraph>
         </ListContainer>
       </ListItem>
+  
       <ListItem>
         <picture>
           <DiFirebase size="3rem" />
@@ -42,10 +46,25 @@ const Technologies = () => (
           <ListTitle>Back-End</ListTitle>
           <ListParagraph>
             Experience with <br />
-            Node, Express and Databases MySQL
+            Node, Express And Java, Spring Boot<br />
+            And Django 
           </ListParagraph>
         </ListContainer>
       </ListItem>
+  
+       <ListItem>
+        <picture>
+          <DiFirebase size="3rem" />
+        </picture>
+        <ListContainer>
+          <ListTitle>Data Base</ListTitle>
+          <ListParagraph>
+            Experience with <br />
+            MySQL, PostgreSQL And Oracle
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+  
     </List>
     <SectionDivider colorAlt />
   </Section>
